816e98dea767167c0203822854fc3033b7ade31e
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-10-30 00:16:14 +0900

    💄 feat: wrap app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from './components/ui/provider.tsx'
 
 // biome-ignore lint: lint/style/noNonNullAssertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
