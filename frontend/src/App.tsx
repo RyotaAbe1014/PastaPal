a2363186c267f99ca322ceddaac5b87f4dc548f9
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-10-30 00:20:06 +0900

    💄 feat: chakraUI setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import { useCounter } from './useCounter'
+import { HStack } from '@chakra-ui/react'
+import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,6 +35,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <HStack>
+        <Button color={'aliceblue'}>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
     </>
   )
 }
