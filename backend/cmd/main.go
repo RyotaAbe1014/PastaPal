eaaf7f3cce1fdb35a496406a4dca90f0316b0db4
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-01 16:57:34 +0900

    ✨ feat: ルーティングを分割できるように修正

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"net/http"
 
+	v1Router "github.com/RyotaAbe1014/Pastapal/internal/presentation/v1"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "True")
+	})
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	// api/v1以下のルーティングを設定
+	g := e.Group("/api/v1")
+	v1Router.Router(g)
+
 	e.Logger.Fatal(e.Start(":8000"))
 }
