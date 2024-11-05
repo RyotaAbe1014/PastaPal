476e3209ade5c8bf545171c5301956f2903aeb62
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-06 01:35:24 +0900

    🎨 style: コードスタイルを改善し、インポートを整理

--- frontend/src/routes/(app)/ingredients.tsx
+++ frontend/src/routes/(app)/ingredients.tsx
@@ -1,24 +1,23 @@
-import { BaseLayout } from '@/components/layout/BaseLayout'
+import { BaseLayout } from "@/components/layout/BaseLayout";
 import {
-  IngredientCategoryCreateForm,
-  IngredientCreateForm,
-} from '@/features/ingredient'
-import { Box } from '@chakra-ui/react'
-import { createFileRoute } from '@tanstack/react-router'
+	IngredientCategoryCreateForm,
+	IngredientCreateForm,
+} from "@/features/ingredient";
+import { Box } from "@chakra-ui/react";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(app)/ingredients')({
-  component: Ingredients,
-})
+export const Route = createFileRoute("/(app)/ingredients")({
+	component: Ingredients,
+});
 
 function Ingredients() {
-  return (
-    <BaseLayout path={'ingredients'}>
-      <Box as="div" display={'flex'} flexDirection={'column'} gap={4}>
-        <IngredientCreateForm />
-        <IngredientCategoryCreateForm />
-        {/* TODO: 食材一覧 */}
-      </Box>
-    </BaseLayout>
-
-  )
+	return (
+		<BaseLayout path={"ingredients"}>
+			<Box as="div" display={"flex"} flexDirection={"column"} gap={4}>
+				<IngredientCreateForm />
+				<IngredientCategoryCreateForm />
+				{/* TODO: 食材一覧 */}
+			</Box>
+		</BaseLayout>
+	);
 }
