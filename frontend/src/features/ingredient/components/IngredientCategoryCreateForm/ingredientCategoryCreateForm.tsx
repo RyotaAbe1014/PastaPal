7caff5a9a857671cbf582543ee70b761c8e1de3b
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-05 22:01:26 +0900

    ✨ feat: IngredientCategoryCreateFormコンポーネントを追加し、エクスポートを設定

---
+++ frontend/src/features/ingredient/components/IngredientCategoryCreateForm/ingredientCategoryCreateForm.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Card, Input } from "@chakra-ui/react";
+import { BsPlus } from "react-icons/bs";
+
+export const IngredientCategoryCreateForm = () => {
+	return (
+		<Card.Root variant={"elevated"}>
+			<Card.Header>
+				<Card.Title fontSize={"xl"} fontWeight={"700"} color={"green.600"}>
+					種別の追加
+				</Card.Title>
+			</Card.Header>
+			<Card.Body display={"flex"} flexDirection={"row"} gap={"3"} pt={2}>
+				<Input placeholder="新しい種別" backgroundColor={"green.100"} />
+				<Button backgroundColor={"green.600"}>
+					<BsPlus />
+					追加
+				</Button>
+			</Card.Body>
+		</Card.Root>
+	);
+};
--- frontend/src/features/ingredient/components/index.ts
+++ frontend/src/features/ingredient/components/index.ts
@@ -1,1 +1,2 @@
 export * from "./IngredientCreateForm";
+export * from "./IngredientCategoryCreateForm";
