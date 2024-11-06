165b2e1d567bb200c969cccd76c77594928da761
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-07 00:25:40 +0900

    ✨ feat: 食材リストテーブルのストーリーを追加し、コンポーネントの構造を整理

---
+++ frontend/src/features/ingredient/components/IngredientListTable/ingredientListTable.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { IngredientListTable } from ".";
+
+const meta: Meta<typeof IngredientListTable> = {
+	component: IngredientListTable,
+	title: "Components/Layout/IngredientListTable",
+};
+
+export default meta;
+type Story = StoryObj<typeof IngredientListTable>;
+
+export const Default: Story = {
+	args: {},
+};
