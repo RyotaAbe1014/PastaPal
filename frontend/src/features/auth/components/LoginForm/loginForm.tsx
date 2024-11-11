61bd76589c13627b3e4ec5f8c70623d217d21e07
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-11 22:36:46 +0900

    ✨ feat: ログインフォームを更新し、GitHubログインボタンを追加

--- frontend/src/features/auth/components/LoginForm/loginForm.tsx
+++ frontend/src/features/auth/components/LoginForm/loginForm.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Field } from "@/components/ui/field";
-import { Card, Input } from "@chakra-ui/react";
+import { Card } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
 
 export const LoginForm = () => {
 	return (
@@ -11,22 +11,10 @@ export const LoginForm = () => {
 				</Card.Title>
 			</Card.Header>
 			<Card.Body display={"flex"} flexDirection={"column"} gap={8} pt={6}>
-				<Field label="メールアドレス">
-					<Input
-						type={"text"}
-						placeholder="example@example.com"
-						backgroundColor={"green.100"}
-					/>
-				</Field>
-				<Field label="パスワード">
-					<Input
-						type="password"
-						placeholder="example@example.com"
-						backgroundColor={"green.100"}
-					/>
-				</Field>
-				<Button type="button" backgroundColor={"green.600"}>
-					ログイン
+
+				<Button type="button" backgroundColor={"black"}>
+					<FaGithub />
+					GitHubでログイン
 				</Button>
 			</Card.Body>
 		</Card.Root>
