7a2098aef231081dca7678badc574f3664f89189
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-14 00:11:26 +0900

    🛂 feat: 認証ルートの追加

--- frontend/src/routes/_app.tsx
+++ frontend/src/routes/_app.tsx
@@ -1,8 +1,20 @@
 import { BaseLayout } from "@/components/layout/BaseLayout";
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app")({
 	component: AppLayout,
+	beforeLoad: async ({ location }) => {
+		const userId = localStorage.getItem("userId");
+		if (!userId) {
+			// APIはcookieのトークンを見ているので、ローカルストレージのユーザー名だけでログイン済みとみなす
+			throw redirect({
+				to: "/login",
+				search: {
+					redirect: location.href,
+				},
+			});
+		}
+	},
 });
 
 function AppLayout() {
