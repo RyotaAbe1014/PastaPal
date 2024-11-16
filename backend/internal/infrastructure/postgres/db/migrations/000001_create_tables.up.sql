f07daf02c855eb7db7889885f59e21b7bb7ffb71
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-16 19:28:01 +0900

    🩹 fix: upとdown間違えたので修正

--- backend/internal/infrastructure/postgres/db/migrations/000001_create_tables.up.sql
+++ backend/internal/infrastructure/postgres/db/migrations/000001_create_tables.up.sql
@@ -1,3 +1,19 @@
-DROP TABLE IF EXISTS ingredients;
+-- 食材種別
+CREATE TABLE IF NOT EXISTS ingredient_categories (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
 
-DROP TABLE IF EXISTS ingredient_categories;
+-- 食材
+CREATE TABLE IF NOT EXISTS ingredients (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  category_id uuid NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES ingredient_categories(id)
+);
