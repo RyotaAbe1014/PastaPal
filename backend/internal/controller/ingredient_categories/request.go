cfadf01b09071ef0158ce73fa7d8633a4320d63a
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-17 22:10:31 +0900

    ✨ feat: 材料カテゴリのリクエストおよびレスポンス構造体を追加

--- backend/internal/controller/ingredient_categories/request.go
+++ backend/internal/controller/ingredient_categories/request.go
@@ -1,1 +1,12 @@
 package ingredient_categories
+
+type CreateIngredientCategoryRequest struct {
+	Name string `json:"name"`
+}
+type UpdateIngredientCategoryRequest struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+type DeleteIngredientCategoryRequest struct {
+	ID int `json:"id"`
+}
