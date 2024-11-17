4c9c540618bb721e22495edc94b92c636db4db1d
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-17 21:58:15 +0900

    🗃️ feat: DB接続関数を修正

--- backend/internal/infrastructure/postgres/db.go
+++ backend/internal/infrastructure/postgres/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/RyotaAbe1014/Pastapal/internal/infrastructure/postgres/db/dbgen"
 	"github.com/RyotaAbe1014/Pastapal/pkg/get_env"
 	"github.com/jackc/pgx/v5"
 )
@@ -16,11 +17,23 @@ var (
 	dbPort     = get_env.Getenv("POSTGRES_PORT", "5432")
 )
 
-func CreateConnection(ctx context.Context) (*pgx.Conn, error) {
+func createConnection(ctx context.Context) (*pgx.Conn, error) {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 	conn, err := pgx.Connect(ctx, connStr)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 	return conn, nil
+}
+
+func getQueries(ctx context.Context) (*dbgen.Queries, *pgx.Conn, error) {
+	conn, err := createConnection(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil, err
+	}
+
+	// connを返却するのは、呼び出し元でCloseするため
+	return dbgen.New(conn), conn, nil
 }
