8d710d7a03321ae560c47194965a685dff5e2fa2
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-17 22:02:42 +0900

    ✨ ✨ feat: 材料カテゴリの大枠のcontroller作成

---
+++ backend/internal/controller/ingredient_categories/handler.go
@@ -0,0 +1,19 @@
+package ingredient_categories
+
+import "github.com/labstack/echo/v4"
+
+func CreateIngredientCategory(c echo.Context) error {
+	return nil
+}
+
+func GetIngredientCategories(c echo.Context) error {
+	return nil
+}
+
+func UpdateIngredientCategory(c echo.Context) error {
+	return nil
+}
+
+func DeleteIngredientCategory(c echo.Context) error {
+	return nil
+}
