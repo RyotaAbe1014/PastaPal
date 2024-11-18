d2eb868882fef9fe5d45b280fd762eacbdf6ab27
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-18 23:59:38 +0900

    ✨ feat: 材料カテゴリレスポンスのIDフィールドをintからstringに変更

--- backend/internal/controller/ingredient_categories/response.go
+++ backend/internal/controller/ingredient_categories/response.go
@@ -1,16 +1,16 @@
 package ingredient_categories
 
 type CreateIngredientCategoryResponse struct {
-	ID   int    `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
 type GetIngredientCategoriesResponse struct {
-	ID   int    `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
 
 type UpdateIngredientCategoryResponse struct {
-	ID   int    `json:"id"`
+	ID   string `json:"id"`
 	Name string `json:"name"`
 }
