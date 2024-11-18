10568b7f51b92013a56f3c6bf32e86d4cfa62c83
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-18 23:59:30 +0900

    ✨ feat: 材料カテゴリハンドラーにサービスを追加し、作成メソッドを実装

--- backend/internal/controller/ingredient_categories/handler.go
+++ backend/internal/controller/ingredient_categories/handler.go
@@ -1,25 +1,39 @@
 package ingredient_categories
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/RyotaAbe1014/Pastapal/internal/service/ingredient_categories"
+	"github.com/labstack/echo/v4"
+)
 
 type IngredientCategoryHandler interface {
-	CreateIngredientCategory(c echo.Context) error
+	CreateIngredientCategory(c echo.Context) (CreateIngredientCategoryResponse, error)
 	GetIngredientCategories(c echo.Context) error
 	UpdateIngredientCategory(c echo.Context) error
 	DeleteIngredientCategory(c echo.Context) error
 }
 
 type ingredientCategoryHandler struct {
-	// ingredientCategoryUseCase IngredientCategoryUseCase
+	ingredientCategoryService ingredient_categories.IngredientCategoryService
 }
 
 // constructorを使用して、controllerの構造体を生成
-func New() IngredientCategoryHandler {
-	return &ingredientCategoryHandler{}
+func New(is ingredient_categories.IngredientCategoryService) IngredientCategoryHandler {
+	return &ingredientCategoryHandler{ingredientCategoryService: is}
 }
 
-func (h *ingredientCategoryHandler) CreateIngredientCategory(c echo.Context) error {
-	return nil
+func (h *ingredientCategoryHandler) CreateIngredientCategory(c echo.Context) (CreateIngredientCategoryResponse, error) {
+	ingredientCategory, err := h.ingredientCategoryService.CreateIngredientCategory(ingredient_categories.CreateIngredientCategoryRequestDTO{
+		Name: c.FormValue("name"),
+	})
+
+	if err != nil {
+		return CreateIngredientCategoryResponse{}, err
+	}
+
+	return CreateIngredientCategoryResponse{
+		ID:   ingredientCategory.ID(),
+		Name: ingredientCategory.Name(),
+	}, nil
 }
 
 func (h *ingredientCategoryHandler) GetIngredientCategories(c echo.Context) error {
