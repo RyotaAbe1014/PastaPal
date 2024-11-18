de86d3703bc26d2908bb325c13e01d1a5e5306e4
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-18 23:55:02 +0900

    ✨ feat: 材料カテゴリサービスの作成と不要なファイルの削除

---
+++ backend/internal/service/ingredient_categories/ingredient_category_service.go
@@ -0,0 +1,59 @@
+package ingredient_categories
+
+import (
+	"errors"
+
+	"github.com/RyotaAbe1014/Pastapal/internal/domain/ingredient_categories"
+	"github.com/google/uuid"
+)
+
+type CreateIngredientCategoryRequestDTO struct {
+	Name string
+}
+
+type IngredientCategoryService interface {
+	CreateIngredientCategory(requestDTO CreateIngredientCategoryRequestDTO) (ingredient_categories.IngredientCategory, error)
+	GetIngredientCategoryByID(id int) (ingredient_categories.IngredientCategory, error)
+	GetAllIngredientCategories() ([]ingredient_categories.IngredientCategory, error)
+	UpdateIngredientCategory(id int, requestDTO CreateIngredientCategoryRequestDTO) (ingredient_categories.IngredientCategory, error)
+	DeleteIngredientCategory(id int) error
+}
+
+type ingredientCategoryService struct {
+	ir ingredient_categories.IIngredientCategoryRepository
+}
+
+func NewIngredientCategoryService(ir ingredient_categories.IIngredientCategoryRepository) IngredientCategoryService {
+	return &ingredientCategoryService{ir: ir}
+}
+
+func (is *ingredientCategoryService) CreateIngredientCategory(requestDTO CreateIngredientCategoryRequestDTO) (ingredient_categories.IngredientCategory, error) {
+	id := uuid.New().String()
+	ingredientCategory, err := ingredient_categories.NewIngredientCategory(id, requestDTO.Name)
+
+	if err != nil {
+		return ingredient_categories.IngredientCategory{},
+			errors.New("failed to create ingredient category")
+	}
+
+	is.ir.CreateIngredientCategory(ingredientCategory)
+
+	return ingredientCategory, nil
+
+}
+
+func (is *ingredientCategoryService) GetIngredientCategoryByID(id int) (ingredient_categories.IngredientCategory, error) {
+	panic("implement me")
+}
+
+func (is *ingredientCategoryService) GetAllIngredientCategories() ([]ingredient_categories.IngredientCategory, error) {
+	panic("implement me")
+}
+
+func (is *ingredientCategoryService) UpdateIngredientCategory(id int, requestDTO CreateIngredientCategoryRequestDTO) (ingredient_categories.IngredientCategory, error) {
+	panic("implement me")
+}
+
+func (is *ingredientCategoryService) DeleteIngredientCategory(id int) error {
+	panic("implement me")
+}
