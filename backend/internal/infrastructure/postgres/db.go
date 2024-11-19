01c0fa90a38c13f0682eb2ba118e6cbd0d676541
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-19 23:54:16 +0900

    ✨ feat: データベース接続とクエリ管理のための新しいメソッドを追加

--- backend/internal/infrastructure/postgres/db.go
+++ backend/internal/infrastructure/postgres/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/RyotaAbe1014/Pastapal/internal/infrastructure/postgres/db/dbgen"
 	"github.com/RyotaAbe1014/Pastapal/pkg/get_env"
@@ -16,6 +17,44 @@ var (
 	dbHost     = get_env.Getenv("POSTGRES_HOST", "localhost")
 	dbPort     = get_env.Getenv("POSTGRES_PORT", "5432")
 )
+
+var (
+	once     sync.Once
+	readOnce sync.Once
+	query    *dbgen.Queries
+	dbcon    *pgx.Conn
+)
+
+// contextからQueriesを取得する。contextにQueriesが存在しない場合は、パッケージ変数からQueriesを取得する
+func GetQuery(ctx context.Context) *dbgen.Queries {
+	txq := getQueriesWithContext(ctx)
+	if txq != nil {
+		return txq
+	}
+	return query
+}
+
+func SetQuery(q *dbgen.Queries) {
+	query = q
+}
+func GetDB() *pgx.Conn {
+	return dbcon
+}
+func SetDB(d *pgx.Conn) {
+	dbcon = d
+}
+
+func NewMainDB() {
+	once.Do(func() {
+		dbcon, err := createConnection(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		q := dbgen.New(dbcon)
+		SetQuery(q)
+		SetDB(dbcon)
+	})
+}
 
 func createConnection(ctx context.Context) (*pgx.Conn, error) {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
@@ -36,4 +75,22 @@ func getQueries(ctx context.Context) (*dbgen.Queries, *pgx.Conn, error) {
 
 	// connを返却するのは、呼び出し元でCloseするため
 	return dbgen.New(conn), conn, nil
+}
+
+type CtxKey string
+
+const (
+	QueriesKey CtxKey = "queries"
+)
+
+func WithQueries(ctx context.Context, q *dbgen.Queries) context.Context {
+	return context.WithValue(ctx, QueriesKey, q)
+}
+
+func getQueriesWithContext(ctx context.Context) *dbgen.Queries {
+	queries, ok := ctx.Value(QueriesKey).(*dbgen.Queries)
+	if !ok {
+		return nil
+	}
+	return queries
 }
