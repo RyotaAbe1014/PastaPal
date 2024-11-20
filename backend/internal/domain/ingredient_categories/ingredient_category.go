c904b153c8f2f673a883448e32a7bba317b6f86f
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-20 23:55:55 +0900

    ♻️ refactor: ドメインモデルにバリデーション追加

--- backend/internal/domain/ingredient_categories/ingredient_category.go
+++ backend/internal/domain/ingredient_categories/ingredient_category.go
@@ -1,6 +1,9 @@
 package ingredient_categories
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type IngredientCategory struct {
 	id         string
@@ -9,11 +12,23 @@ type IngredientCategory struct {
 	updated_at time.Time
 }
 
+// constructor
 func NewIngredientCategory(id string, name string) (IngredientCategory, error) {
-	// TODO: add validation
+	if err := validateName(name); err != nil {
+		return IngredientCategory{}, err
+	}
 	return IngredientCategory{id: id, name: name}, nil
 }
 
+// 永続化層から取得したデータをドメイン層に変換する
+func NewIngredientCategoryFromRepository(id string, name string, created_at time.Time, updated_at time.Time) (IngredientCategory, error) {
+	if err := validateName(name); err != nil {
+		return IngredientCategory{}, err
+	}
+	return IngredientCategory{id: id, name: name, created_at: created_at, updated_at: updated_at}, nil
+}
+
+// getter
 func (i IngredientCategory) ID() string {
 	return i.id
 }
@@ -22,4 +37,14 @@ func (i IngredientCategory) Name() string {
 	return i.name
 }
 
-// TODO: add private validate method
+// validation
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("食材種別名は必須です")
+	}
+
+	if len(name) > 30 {
+		return errors.New("食材種別は30文字以内で入力してください")
+	}
+	return nil
+}
