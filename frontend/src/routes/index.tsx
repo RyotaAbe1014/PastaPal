3c78c50ea3d8de7d4d30d4302d0854a68220c892
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-21 00:04:03 +0900

    ✨ feat: 認証ステータスに基づいてリダイレクト処理を追加

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,51 +1,25 @@
-import { Button } from "@/components/ui/button";
-import { createFileRoute } from "@tanstack/react-router";
-import { atom, useAtom } from "jotai";
-import useSWR from "swr";
+import { ApiClient } from "@/api/client";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+
+type AuthStatusResponse = {
+	isAuthenticated: boolean;
+};
 
 export const Route = createFileRoute("/")({
-	component: Index,
-});
-
-const countAtom = atom(0);
-
-export function Index() {
-	const [count, setCount] = useAtom(countAtom);
-
-	const { isLoading, isValidating, data } = useSWR(
-		"http://localhost:8000/health",
-		async (url) => {
-			const res = await fetch(url);
-			return res;
-		},
-	);
-
-	return (
-		<>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<Button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</Button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<div className="card">
-				using Jotai
-				{/* {counter} */}
-			</div>
-
-			<div className="card">
-				<h2>SWR</h2>
-				{isLoading ? (
-					<div>Loading...</div>
-				) : isValidating ? (
-					<div>Validating...</div>
-				) : (
-					<div>{data ? "Success" : "Failed"}</div>
-				)}
-			</div>
-		</>
-	);
-}
+	beforeLoad: async () => {
+		const api = ApiClient();
+		const response = await api.Get<undefined, AuthStatusResponse>(
+			"/auth/github/status",
+		);
+		if (!response.isAuthenticated) {
+			// APIはcookieのトークンを見ているので、ローカルストレージのユーザー名だけでログイン済みとみなす
+			throw redirect({
+				to: "/login",
+			});
+		}
+		throw redirect({
+			to: "/ingredients",
+		});
+	}
+},
+);
