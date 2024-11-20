8a30802fe9bec033e905b8cb259b804360ff7ce1
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-20 23:43:49 +0900

    ✨ feat: 食材種別の取得機能を実装し、コンテキストを使用するように変更

--- backend/internal/presentation/v1/router.go
+++ backend/internal/presentation/v1/router.go
@@ -111,6 +111,7 @@ func Router(g *echo.Group) {
 	// 食材種別管理
 	g.POST("/ingredient-categories", func(c echo.Context) error {
 		ctx := c.Request().Context()
+		// TODO: ここのDIはもっとスマートに書けるはず、他のingredientCategory関連の処理と共通化できるはず
 		ingredientCategoryRepository := repository.NewIngredientCategoryRepository()
 		ingredientCategoryService := ingredient_categories_service.NewIngredientCategoryService(ingredientCategoryRepository)
 		ingredientCategoryController := ingredient_categories_controller.NewIngredientCategoryController(ingredientCategoryService)
@@ -132,7 +133,18 @@ func Router(g *echo.Group) {
 	})
 
 	g.GET(("/ingredient-categories"), func(c echo.Context) error {
-		return c.String(http.StatusOK, "ingredient-categories")
+		ctx := c.Request().Context()
+		ingredientCategoryRepository := repository.NewIngredientCategoryRepository()
+		ingredientCategoryService := ingredient_categories_service.NewIngredientCategoryService(ingredientCategoryRepository)
+		ingredientCategoryController := ingredient_categories_controller.NewIngredientCategoryController(ingredientCategoryService)
+
+		result, err := ingredientCategoryController.GetIngredientCategories(ctx)
+
+		if err != nil {
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		return c.JSON(http.StatusOK, result)
 	})
 
 	g.GET("/ingredient-categories/:id", func(c echo.Context) error {
