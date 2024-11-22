0bb687153c834ce7f8cc1353ca871375a25d9b1b
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-22 23:40:17 +0900

    ♻️ refactor: APIクライアントのリクエスト処理を整理

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,36 +1,4 @@
 export const ApiClient = () => {
-	const settings = {
-		BaseUrl: import.meta.env.VITE_ENV_API_URL,
-	};
-	const request = async <RequestType = undefined, ResponseType = unknown>(
-		path: string,
-		method: string,
-		params?: RequestType,
-	): Promise<ResponseType> => {
-		const url = `${settings.BaseUrl}${path}`;
-		const options: RequestInit = {
-			method,
-			credentials: "include",
-			mode: "cors",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: params ? JSON.stringify(params) : undefined,
-		};
-		const response = await fetch(url, options);
-		return handleResponse<ResponseType>(response);
-	};
-
-	const handleResponse = async <ResponseType>(
-		response: Response,
-	): Promise<ResponseType> => {
-		if (!response.ok) {
-			// TODO: ここのエラーハンドリングは適切なものに変更する
-			throw new Error("fetch failed");
-		}
-		return response.json() as Promise<ResponseType>;
-	};
-
 	const Get = async <RequestType = undefined, ResponseType = unknown>(
 		path: string,
 		params?: RequestType,
@@ -79,4 +47,36 @@ const requestToUrlSearch = <
 		}
 	});
 	return searchParams;
+};
+
+const request = async <RequestType = undefined, ResponseType = unknown>(
+	path: string,
+	method: string,
+	params?: RequestType,
+): Promise<ResponseType> => {
+	const settings = {
+		BaseUrl: import.meta.env.VITE_ENV_API_URL,
+	};
+	const url = `${settings.BaseUrl}${path}`;
+	const options: RequestInit = {
+		method,
+		credentials: "include",
+		mode: "cors",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: params ? JSON.stringify(params) : undefined,
+	};
+	const response = await fetch(url, options);
+	return handleResponse<ResponseType>(response);
+};
+
+const handleResponse = async <ResponseType>(
+	response: Response,
+): Promise<ResponseType> => {
+	if (!response.ok) {
+		// TODO: ここのエラーハンドリングは適切なものに変更する
+		throw new Error("fetch failed");
+	}
+	return response.json() as Promise<ResponseType>;
 };
