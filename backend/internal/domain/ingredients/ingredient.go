4baed572d8fdb97f46adb43181487f69c2f63395
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-26 22:47:58 +0900

    ✨ feat: 食材を表すIngredient構造体とそのコンストラクタを追加し、バリデーション機能を実装

---
+++ backend/internal/domain/ingredients/ingredient.go
@@ -0,0 +1,51 @@
+package ingredients
+
+import (
+	"errors"
+	"time"
+)
+
+type Ingredient struct {
+	id                       string
+	ingredient_categories_id string
+	name                     string
+	created_at               time.Time
+	updated_at               time.Time
+}
+
+// constructor
+func NewIngredient(id string, name string) (Ingredient, error) {
+	if err := validateName(name); err != nil {
+		return Ingredient{}, err
+	}
+	return Ingredient{id: id, name: name}, nil
+}
+
+// 永続化層から取得したデータをドメイン層に変換する
+func NewIngredientFromRepository(id string, name string, created_at time.Time, updated_at time.Time) (Ingredient, error) {
+	if err := validateName(name); err != nil {
+		return Ingredient{}, err
+	}
+	return Ingredient{id: id, name: name, created_at: created_at, updated_at: updated_at}, nil
+}
+
+// getter
+func (i Ingredient) ID() string {
+	return i.id
+}
+
+func (i Ingredient) Name() string {
+	return i.name
+}
+
+// validation
+func validateName(name string) error {
+	if name == "" {
+		return errors.New("食材名は必須です")
+	}
+
+	if len(name) > 90 {
+		return errors.New("食材名は90文字以内で入力してください")
+	}
+	return nil
+}
