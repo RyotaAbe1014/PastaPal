aad7710a56b6e932aaec9c4b18086513ca77f31e
RyotaAbe1014 <bm1000bmmm@gmail.com>
2024-11-26 22:36:11 +0900

    ✨ feat: APIエラーハンドリングの条件式のフォーマットを改善

--- frontend/src/routes/_app.tsx
+++ frontend/src/routes/_app.tsx
@@ -11,7 +11,7 @@ export const Route = createFileRoute("/_app")({
 			"/auth/github/status",
 		);
 
-		if(isApiError(response)) {
+		if (isApiError(response)) {
 			throw redirect({
 				to: "/login",
 			});
